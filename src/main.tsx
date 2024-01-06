145f9734b769eda38af457ef438004dbd8709ef6
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-01-06 14:20:01 +0300

    Fixed the nav menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import { Dashboard } from '@/pages/dashboard';
+import { Navbar } from '@/components/navbar';
+import { Landing } from '@/pages/landing';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Navbar />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
